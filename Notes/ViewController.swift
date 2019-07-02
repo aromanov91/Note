fb9afa3524ead2f133737bd22ff390af049b4ece
aromanov07 <aromanov07@gmail.com>
2019-07-02 23:47:33 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Aleksandr Romanov on 02/07/2019.
+//  Copyright © 2019 Aleksandr Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
