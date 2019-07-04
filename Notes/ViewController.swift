eea3ae00ef9404a2172c75d6a0fdfe94975c84ba
Alexandr Romanov <aromanov07@gmail.com>
2019-07-04 11:27:21 +0300

    New shemes

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Создаем баннер
+    private let baseApp: UIView = {
+        let baseApp = UIView()
+        baseApp.translatesAutoresizingMaskIntoConstraints = false
+        baseApp.backgroundColor = UIColor.red
+        return baseApp
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Выборо заголовка в зависимости от платной и бесплатной версии
+        if isFreeVersion {
+            self.title = "Бесплатая версия"
+        } else {
+            self.title = "Платная версия"
+        }
+        
+        // Загружается красный баннер на бесплатной версии
+        if isFreeVersion {
+            view.addSubview(baseApp)
+            createBaseAppConstraint()
+        }
+        
     }
+    
+    // Констрейнты для баннера
+    private func createBaseAppConstraint() {
+        baseApp.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        baseApp.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        baseApp.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        baseApp.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+
+    
 
 
 }
